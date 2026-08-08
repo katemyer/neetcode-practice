10f0c152098853c45b0af6a5e6cefdc98a29473a
kate.m.phd <51521448+katemyer@users.noreply.github.com>
2026-08-08 10:53:39 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 33
+print(type(variable))
+
+variable = 22.1
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "corra"
+print(type(variable))
+
+variable = [5,4,1]
+print(type(variable))
