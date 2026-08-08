f5627130c9bb3f07dadaae1c97a99c22fb07c828
kate.m.phd <51521448+katemyer@users.noreply.github.com>
2026-08-08 10:52:54 -0700

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,11 @@
+integer_type = 11
+float_type = 11.2
+bool_type = False
+string_type = "miguelito"
+list_type = [1,3,3,7]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
