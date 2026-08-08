e8ff5cc978f55124b81c1fcbbb0b6fd9f222af8b
kate.m.phd <51521448+katemyer@users.noreply.github.com>
2026-08-08 10:48:53 -0700

    Add: python-multiple-assignment - submission-0

---
+++ Python For Beginners/python-multiple-assignment/submission-0.py
@@ -0,0 +1,14 @@
+msg2, msg1 = "World", "Hello"
+msg3, msg4, msg5 = "My", "Name", "Is" 
+
+# Don't change the code above this line
+
+
+
+
+# Don't change the code below this line
+print(msg1)
+print(msg2)
+print(msg3)
+print(msg4)
+print(msg5)
